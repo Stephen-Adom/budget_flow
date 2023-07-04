613a733974fba7a2586e374fad4761c819b0a802
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-07-04 09:11:34 +0000

    set up models and tables

---
+++ app/models/category.rb
@@ -0,0 +1,7 @@
+class Category < ApplicationRecord
+  belongs_to :author, class_name: "User", foreign_key: :author_id
+  has_many :category_expenses
+  has_many :expenses, through: :category_expenses
+
+  validates :name, presence: true
+end
