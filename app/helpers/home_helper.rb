0550ab3fbed4c64fd5bd2af423216f230beae689
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-07-04 16:04:33 +0000

    update recent transaction list

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,6 @@
 module HomeHelper
+  def recent_expenses
+    CategoryExpense.includes(:category, :expense).where(expense: { author: current_user, created_at: 1.week.ago..Time.now })
+      .order(created_at: :desc)
+  end
 end
